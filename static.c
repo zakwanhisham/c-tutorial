ab7faf5b6135d7f1068d596d80c1ed8ffb4f44c4
zakwan hisham <hishamzakwan@gmail.com>
2023-06-28 13:50:45 +0800

    Static

---
+++ static.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum(int num) {
+  static int count = 0;
+  count += num;
+  return count;
+}
+
+int main() {
+  printf("%d ", sum(55));
+  printf("%d ", sum(45));
+  printf("%d ", sum(50));
+
+  return 0;
+}
